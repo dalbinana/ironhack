ba4ce838f466a60c7a0ede493b6045d5ae4eda3f
daniel_albinyana <dani@commonpeoplei.com>
2016-02-03 14:53:57 +0100

    added errors in the front

--- timetracking/app/controllers/entries_controller.rb
+++ timetracking/app/controllers/entries_controller.rb
@@ -1,15 +1,26 @@
 class EntriesController < ApplicationController
-
   def index
     @project = Project.find params[:project_id]
     # @entries = @project.entries
     @entries = @project.entries.where("created_at >= ? AND created_at <= ?", Date.today.beginning_of_month, Date.today.end_of_month)
   end
-  
+
   def new
     @project = Project.find params[:project_id]
     @entry = @project.entries.new
   end
-
-
+  def create
+    @project = Project.find params[:project_id]
+    @entry = @project.entries.new entry_params
+    if @entry.save
+      redirect_to action: 'index', controller:'entries', project_id: @project.id
+    else
+      render 'new'
+     end
+    end
+private
+	def entry_params
+		params.require(:entry).permit(:hours, :minutes, :date)
+	end    
 end
+
