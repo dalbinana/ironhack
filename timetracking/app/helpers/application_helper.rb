efb9b84b32d79464d0dc9a677f5658921087dd06
daniel_albinyana <dani@commonpeoplei.com>
2016-02-04 10:34:30 +0100

    FLASH MESSAGES: Per poder donar-li estils, fem això.

--- timetracking/app/helpers/application_helper.rb
+++ timetracking/app/helpers/application_helper.rb
@@ -1,9 +1,22 @@
 module ApplicationHelper
 	def flash_message
-       if flash[:alert]
-         flash[:alert]
-       elsif flash[:notice]
-         flash[:notice]
-       end
+		if flash[:alert]
+      content_tag :div, class: 'message alert' do
+        content_tag :p do
+          flash[:alert]
+        end
+      end
+  elsif flash[:notice]
+      content_tag :div, class: 'message notice' do
+        content_tag :p do
+          flash[:notice]
+        end
+      end
+  end
 end
+
+   
+
+
 end
+
