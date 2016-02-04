6f79fb121d3378f38130e87b7a725127949a7707
daniel_albinyana <dani@commonpeoplei.com>
2016-02-04 14:53:15 +0100

    Refractoring

--- timetracking/app/helpers/application_helper.rb
+++ timetracking/app/helpers/application_helper.rb
@@ -1,32 +1,22 @@
 module ApplicationHelper
-	def flash_message
-		if flash[:alert]
-      content_tag :div, class: 'message alert' do
-        content_tag :p do
-          flash[:alert]
-        end
-      end
-  elsif flash[:notice]
-      content_tag :div, class: 'message notice' do
-        content_tag :p do
-          flash[:notice]
-        end
-      end
+
+  def flash_message
+    display_message(:alert) || display_message(:notice)
   end
+
+  private
+  def display_message(type)
+    if flash[type]
+      content_tag :div, class: type do
+        content_tag :p do
+          flash[type]
+        end
+      end
+    end
+  end
+
 end
-end
-# Solucion pijoteras para hacer lo mismo
-#      def flash_message
-#         unless flash.empty?
-#             content_tag :div, class: 'message' do
-#                 flash.each do |key, mess|
-#                     content_tag :span, class: "#{key}" do
-#                         mess
-#                     end
-#                 end
-#             end
-#         end
-#     end    
+
 
 
 
